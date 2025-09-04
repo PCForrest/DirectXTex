6eff30f53a9cf389dd2f015764cc9114e2b3ae98
Pete Forrest <60230910+PCForrest@users.noreply.github.com>
2025-09-04 07:45:02 +0100

    Add Premake.

---
+++ DirectXTex/premake5.lua
@@ -0,0 +1,75 @@
+project "DirectXTex"
+	kind "StaticLib"
+
+	language "C++"
+	cppdialect "C++17"
+	staticruntime "Off"
+	location "%{wks.location}/Libs/Vendor/%{prj.name}"
+
+	targetdir (bin_dir .. "/Libs/Vendor/%{prj.name}")
+	objdir (int_dir .. "/Libs/Vendor/%{prj.name}")
+
+	pchheader "DirectXTexP.h"
+	pchsource "DirectXTexUtil.cpp"
+
+	files
+	{
+		"%{prj.location}/premake5.lua",
+		"%{prj.location}/*.h",
+		"%{prj.location}/*.inl",
+		"%{prj.location}/*.cpp"
+	}
+
+	removefiles
+	{
+		"%{prj.location}/premake5.lua",
+		"%{prj.location}/d3dx12.h",
+		"%{prj.location}/DirectXTexD3D12.cpp",
+	}
+
+	includedirs
+	{
+		"%{prj.location}",
+		"%{prj.location}/Shaders/Compiled",
+	}
+
+	dependson
+	{
+		-- Nil
+	}
+
+	links
+	{
+		-- Nil
+	}
+
+	defines
+	{
+		"_UNICODE",
+		"UNICODE",
+		"WIN32",
+		"_LIB",
+		"_WIN7_PLATFORM_UPDATE",
+		"_WIN32_WINNT=0x0601",
+		"_CRT_STDIO_ARBITRARY_WIDE_SPECIFIERS",
+	}
+
+	filter "system:windows"
+		systemversion "latest"
+
+	filter "configurations:Debug"
+		defines { "_DEBUG" }
+		runtime "Debug"
+		symbols "on"
+
+	filter "configurations:Release"
+		defines { "NDEBUG" }
+		runtime "Release"
+		optimize "on"
+
+	filter "configurations:Dist"
+		defines { "NDEBUG" }
+		runtime "Release"
+		optimize "on"
+
+	filter {}
